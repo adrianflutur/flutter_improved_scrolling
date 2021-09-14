28014a4a40b9d18909a72be83f235b4566d49878
Adrian Flutur <ady_flutur@yahoo.com>
2021-09-14 18:37:09 +0300

    Added mouse wheel scroll throttle

---
+++ lib/src/throttler.dart
@@ -0,0 +1,21 @@
+/// Throttle some action by a specified amount of milliseconds
+class Throttler {
+  /// Default throttle value
+  Throttler(this._throttleTimeMs);
+
+  final int _throttleTimeMs;
+  int? _lastRunTimeMs;
+
+  /// Run the function which needs to be throttled
+  void run(void Function() action) {
+    if (_lastRunTimeMs == null) {
+      action();
+      _lastRunTimeMs = DateTime.now().millisecondsSinceEpoch;
+    } else {
+      if (DateTime.now().millisecondsSinceEpoch - _lastRunTimeMs! > _throttleTimeMs) {
+        action();
+        _lastRunTimeMs = DateTime.now().millisecondsSinceEpoch;
+      }
+    }
+  }
+}
